87c3e9faad6b079ca73af10dfbbbd71e4b4888e5
shuyuan <seanshu96@gmail.com>
2020-07-16 18:22:52 +0800

    fix: remove url

--- electron/main.js
+++ electron/main.js
@@ -1,22 +1,27 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const url = require('url');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
 let mainWindow;
-function createWindow () {
-  const startUrl = 'http://dev.panyun.qihoo.net:780/login'
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+function createWindow() {
+  const startUrl = "www.baidu.com";
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 768,
+    minWidth: 1200,
+    minHeight: 760,
+  });
   mainWindow.loadURL(startUrl);
-  mainWindow.on('closed', function () {
+  mainWindow.on("closed", function () {
     mainWindow = null;
   });
 }
-app.on('ready', createWindow);
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
+app.on("ready", createWindow);
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
-app.on('activate', function () {
+app.on("activate", function () {
   if (mainWindow === null) {
     createWindow();
   }
